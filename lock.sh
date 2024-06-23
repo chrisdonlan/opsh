7d444e4df5db33717f64c839bbdb7addd5af71b5
Christopher Donlan <christopher.j.donlan@gmail.com>
2024-06-23 08:04:33 -0400

    updated lock.sh w/ output file mode.

--- lock.sh
+++ lock.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 usage(){
 cat << USAGE
-`basename $0` FILE
+`basename $0` FILE [DECRYPTED_OUTPUT_FILE]
 
-	If the file ends in '.enc', it will be decrypted onto standard out. Otherwise, 
+	If the file ends in '.enc', it will be decrypted onto standard out. Otherwise,
  	the file will be encrypted to FILE.enc.
 
 	NOTE: the original file is not destroyed.
 
-	Cookbook: 
-	
+	Cookbook:
+
 		# recover secrets without touching the hard drive
 		source <(echo "your-password" | ./enc.sh encrypted-file.enc)
 
@@ -17,11 +17,14 @@ USAGE
 exit 0
 }
 [ -z "$1" ] && usage
+DECRYPTED="$2"
 
 read -r -s -p "password: " PASSWORD
 if ! [[ "$1" =~ [.]enc$ ]]; then
 	echo
 	openssl enc -aes-256-cbc -iter 10 -in "$1" -out "$1.enc" -k ${PASSWORD}
 else
-	openssl enc -d -aes-256-cbc -iter 10 -in "$1" -out /dev/stdout -k ${PASSWORD}
+	[ -n "${DECRYPTED}" ] && echo
+	openssl enc -d -aes-256-cbc -iter 10 -in "$1" -out "${DECRYPTED:=/dev/stdout}" -k ${PASSWORD}
+
 fi
